2a7c03c18f357712473093cd6a1d210faca2a30b
zipeng <601476275@qq.com>
2019-03-14 15:41:57 +0800

    ok

--- hreader/common.h
+++
@@ -1,8 +0,0 @@
-typedef struct BinTree{
-    char val;
-    struct BinTree *lchild;
-    struct BinTree *rchild;
-}BTree,TNode;
-
-TNode *createBT(char *pre,char *in,int L1,int R1,int L2,int R2);
-void inOrder(BTree *p);
